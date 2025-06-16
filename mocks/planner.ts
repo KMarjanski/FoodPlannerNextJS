d04e5680431d5447a54f1685609d8524113db403
Kacper Marjanski <kacpermarjanskipublic@gmail.com>
2025-06-16 13:19:16 +0200

    Remove mock planner data and update .gitignore for structure.txt

--- mocks/planner.ts
+++
@@ -1,9 +0,0 @@
-export const getData = {
-  MON: ["Tortilla z gyro-tofu"],
-  TUE: ["Tortilla z gyro-tofu"],
-  WED: [],
-  THU: [],
-  FRI: ["Makaron z kurkami"],
-  SAT: ["Makaron z kurkami"],
-  SUN: [],
-};
